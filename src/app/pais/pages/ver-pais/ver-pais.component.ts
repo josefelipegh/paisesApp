4e119d4b04627953a2354b4b381cb09b34961f2a
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2021-05-04 15:42:10 -0300

    Rxjs - SwitchMap Operator

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -10,20 +11,31 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor( 
+  constructor(
     private activatedRoute: ActivatedRoute,
     private _paisService: PaisService
-     ) { }
+  ) { }
 
   ngOnInit(): void {
+    // id Viene de params del observable. switchMap es un operador que recibe params, porque lo infiere, y debe retornar un observable.
+    // Con esto logramos que con el pipe y el operador switchMap retornemos el observable que necesitamos subcribirnos.
     this.activatedRoute.params
-    // id Viene de params del observable.
-      .subscribe( ({id}) => {
-        this._paisService.getPaisPorAlpha(id)
-          .subscribe(pais => {
-            console.log(pais);
-          })
-      })
+      .pipe(
+        switchMap(({ id }) => this._paisService.getPaisPorAlpha(id))
+      )
+      .subscribe(resp => {
+        console.log(resp);
+      });
+
+
+    // this.activatedRoute.params
+    // // id Viene de params del observable.
+    //   .subscribe( ({id}) => {
+    //     this._paisService.getPaisPorAlpha(id)
+    //       .subscribe(pais => {
+    //         console.log(pais);
+    //       })
+    //   });
   }
 
 }
