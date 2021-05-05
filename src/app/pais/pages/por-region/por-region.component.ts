e87192134503c37d6570a8b8186251e83d242df3
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2021-05-05 14:58:39 -0300

    ngClass, class, [class.], [class] y mostrar paises por region

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `
+    button{
+      margin-right: 5px;
+    }
+    `
   ]
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActive: string = '';
+
+  hayError: boolean = false;
+  paises  : Country[] = [];
+
+  constructor( private _countryServices: PaisService ) { }
+
+  getClaseCSS(region:string):string{
+    return (region === this.regionActive) 
+              ? 'btn btn-primary' 
+              : 'btn btn-outline-primary';
+  }
 
   ngOnInit(): void {
+  }
+
+  activateRegion( region: string ){
+    this.regionActive = region;
+    this.hayError = false;
+    this._countryServices.buscarRegion(region)
+        .subscribe(paises => {
+          this.paises = paises;
+        },
+        (err) => {
+          this.hayError = true;
+          this.paises = [];
+        });
   }
 
 }
