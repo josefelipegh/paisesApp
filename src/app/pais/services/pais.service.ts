bf51f9c805dbe68f139c6f56ff0923e4e18cc3d8
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2021-05-12 21:04:46 -0300

    Animate css

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl: string = 'https://restcountries.eu/rest/v2';
+  
+  get httpParams (): HttpParams {
+    return new HttpParams().set('fields', 'name;capital;alpha2Code;flag;population');
+  }
 
   constructor( private http: HttpClient ) { }
 
@@ -16,14 +21,14 @@ export class PaisService {
     
     const url = `${this.apiUrl}/name/${termino}`;
     
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.httpParams});
   }
 
   buscarCapital(termino:string):Observable<Country[]>{
 
     const url = `${this.apiUrl}/capital/${termino}`;
 
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.httpParams});
   }
 
   getPaisPorAlpha(id:string):Observable<Country>{
@@ -34,9 +39,13 @@ export class PaisService {
   }
 
   buscarRegion( region:string ):Observable<Country[]>{
+
     const url = `${this.apiUrl}/region/${region}`;
 
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params: this.httpParams})
+              .pipe(
+                tap(console.log)
+                );
   }
 
 }
